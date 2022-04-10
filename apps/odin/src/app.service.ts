1b17f4f65e7013280cd90488a7615c8efcbb8432
An Duong <an@ninetysix.io>
2022-04-10 18:24:00 -0400

    Add server setup

---
+++ apps/odin/src/app.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  async getHello(): Promise<{ message: string }> {
+    return {
+      message: 'Hello World',
+    };
+  }
+}
