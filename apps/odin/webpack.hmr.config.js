cb2ed51bb567a849a8c1fdee4b307d829b7de7ce
An Duong <an@ninetysix.io>
2022-04-11 01:55:52 -0400

    Chores: Clean up

--- apps/odin/webpack.hmr.config.js
+++ apps/odin/webpack.hmr.config.js
@@ -1,14 +1,13 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const nodeExternals = require("webpack-node-externals");
-const { RunScriptWebpackPlugin } = require("run-script-webpack-plugin");
+const nodeExternals = require('webpack-node-externals');
+const { RunScriptWebpackPlugin } = require('run-script-webpack-plugin');
 
 module.exports = function (options, webpack) {
   return {
     ...options,
-    entry: ["webpack/hot/poll?100", options.entry],
+    entry: ['webpack/hot/poll?100', options.entry],
     externals: [
       nodeExternals({
-        allowlist: ["webpack/hot/poll?100"],
+        allowlist: ['webpack/hot/poll?100'],
       }),
     ],
     plugins: [
