7d69133cf23b65ed74a10e248ce5a0260d076a90
An Duong <an.duong@budee.org>
2022-04-11 01:59:53 -0400

    Update eslint configuration

--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -1,5 +1,6 @@
 module.exports = {
   root: true,
+  ignorePatterns: ["*.js"],
   extends: ["next", "prettier"],
   settings: {
     next: {
--- packages/config/eslint-react.js
+++ packages/config/eslint-react.js
@@ -1,6 +1,7 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
+  ignorePatterns: ["*.js"],
   settings: {
     react: {
       version: "detect",
