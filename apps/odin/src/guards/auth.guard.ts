2d4223d8e19df18c2ec8522e53bd626c56c8bd3d
An Duong <an@ninetysix.io>
2022-04-11 02:35:09 -0400

    Clean up resolver

--- apps/odin/src/guards/auth.guard.ts
+++ apps/odin/src/guards/auth.guard.ts
@@ -5,45 +5,13 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 
 @Injectable()
 export class GraphqlPassportAuthGuard extends AuthGuard('jwt') {
-  _roles: string[] = ['USER'];
-
-  constructor(roles?: string | string[]) {
+  constructor() {
     super();
-    if (roles) {
-      this._roles = Array.isArray(roles) ? roles : [roles];
-    }
-  }
-
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    await super.canActivate(context);
-    const ctx = GqlExecutionContext.create(context);
-    const req = ctx.getContext().req;
-
-    // check if user has access by validating that he has the required role
-    if (Array.isArray(this._roles)) {
-      for (const requiredRole of this._roles) {
-        if (this.hasAccess(req.user.roles, requiredRole)) {
-          return true;
-        }
-      }
-    }
-    return false;
   }
 
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
     const req = ctx.getContext().req;
     return req;
-  }
-
-  private hasAccess(roles, requiredRole): boolean {
-    if (Array.isArray(roles)) {
-      const adminFoundIndex = roles.findIndex(
-        (role: string) => role.toUpperCase() === requiredRole,
-      );
-      if (adminFoundIndex >= 0) {
-        return true;
-      }
-    }
   }
 }
--- apps/odin/src/modules/users/user.module.ts
+++ apps/odin/src/modules/users/user.module.ts
@@ -3,10 +3,9 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from '@odin/data.models/users/schema';
 import { UsersResolver } from './users.resolver';
-import { UsersService } from './users.service';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
-  providers: [UsersResolver, UsersService, GraphqlPassportAuthGuard],
+  providers: [UsersResolver, GraphqlPassportAuthGuard],
 })
 export class UsersModule {}
