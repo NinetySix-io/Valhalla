2d1f82389278c0cbed851a04e5a7c55feae4e13b
An Duong <an.duong@budee.org>
2022-04-11 02:35:09 -0400

    Clean up resolver

--- apps/odin/src/modules/users/users.resolver.ts
+++ apps/odin/src/modules/users/users.resolver.ts
@@ -1,29 +1,12 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
 import { UseGuards } from '@nestjs/common';
-
-import { UsersService } from './users.service';
-import { GraphqlPassportAuthGuard } from '@odin/guards/auth.guard';
-import { PaginatedResponse } from '@odin/graphql/types/paginated.response.type';
-import { User } from './graphql/user.type';
-import { Roles } from '@odin/decorators/roles.decorator';
+import { Query, Resolver } from '@nestjs/graphql';
 import { CurrentUser } from '@odin/decorators/current.user.decorator';
-import { UserQueryArgs } from './graphql/pagination.input';
-
-const PaginatedUserResponse = PaginatedResponse(User);
-type PaginatedUserResponse = InstanceType<typeof PaginatedUserResponse>;
+import { GraphqlPassportAuthGuard } from '@odin/guards/auth.guard';
+import { User } from './graphql/user.type';
 
 @Resolver()
 export class UsersResolver {
-  constructor(private readonly usersService: UsersService) {}
-
-  @Query(() => PaginatedUserResponse)
-  @Roles('admin')
-  @UseGuards(new GraphqlPassportAuthGuard('ADMIN'))
-  async users(
-    @Args() queryArgs: UserQueryArgs,
-  ): Promise<PaginatedUserResponse> {
-    return await this.usersService.getUsers(queryArgs);
-  }
+  constructor() {}
 
   @Query(() => User)
   @UseGuards(GraphqlPassportAuthGuard)
