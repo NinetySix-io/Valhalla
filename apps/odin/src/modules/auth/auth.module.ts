bb192e7c96e096a7103f303705bba74b2d5767bc
An Duong <an.duong@budee.org>
2022-04-10 23:46:17 -0400

    Temporary disable oauth strategies

--- apps/odin/src/modules/auth/auth.module.ts
+++ apps/odin/src/modules/auth/auth.module.ts
@@ -1,17 +1,17 @@
 import { AuthController } from './auth.controller';
 import { AuthService } from './services/auth.service';
 import { Environment } from '@odin/config/environment';
-import { FacebookService } from './services/facebook.service';
-import { FacebookStrategy } from './strategies/facebook.strategy';
-import { GithubStrategy } from './strategies/github.strategy';
-import { GoogleStrategy } from './strategies/google.strategy';
+// import { FacebookService } from './services/facebook.service';
+// import { FacebookStrategy } from './strategies/facebook.strategy';
+// import { GithubStrategy } from './strategies/github.strategy';
+// import { GoogleStrategy } from './strategies/google.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PassportModule } from '@nestjs/passport';
-import { TwitterStrategy } from './strategies/twitter.strategy';
+// import { TwitterStrategy } from './strategies/twitter.strategy';
 import { UserSchema } from '@odin/data.models/users/schema';
 import { UserService } from './services/user.service';
 
