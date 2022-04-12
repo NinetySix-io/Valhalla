2c546221e587ddda63659d3bf83cba1e7643ca79
An Duong <an@ninetysix.io>
2022-04-12 05:15:18 -0400

    Fix Odin App Test

--- apps/odin/src/app.controller.spec.ts
+++ apps/odin/src/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { AppController } from '@odin/app.controller';
-import { AppService } from '@odin/app.service';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -9,16 +9,15 @@ describe('AppController', () => {
     const app: TestingModule = await Test.createTestingModule({
       imports: [],
       controllers: [AppController],
-      providers: [AppService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', async () => {
-      expect(await appController.getHello()).toEqual({
-        message: 'Hello World',
+    it('Succeed', async () => {
+      expect(appController.getStatus()).toEqual({
+        status: 'ok',
       });
     });
   });
