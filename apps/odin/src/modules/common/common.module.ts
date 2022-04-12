de7003bc75c260726f529f9394ecb113b6d250c2
An Duong <an.duong@budee.org>
2022-04-12 05:29:07 -0400

    Remove modules/common

--- apps/odin/src/modules/common/common.module.ts
+++
@@ -1,6 +0,0 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-
-@Module({})
-export class CommonModule implements NestModule {
-  configure(_consumer: MiddlewareConsumer) {}
-}
