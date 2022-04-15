f8cbecf5ec5d0bb685f09143470db3b3c2ffe057
An Duong <an.duong@budee.org>
2022-04-14 23:40:43 -0400

    Add wdyr

--- apps/heimdallr/src/pages/index.tsx
+++ apps/heimdallr/src/pages/index.tsx
@@ -1,10 +1,13 @@
+import { Layout } from "@heimdallr/layout";
 import cx from "clsx";
 import styles from "../styles/home.module.css";
 
 export default function Home() {
   return (
-    <div className={cx(styles.container)}>
-      <h1>Heimdallr</h1>
-    </div>
+    <Layout>
+      <div className={cx(styles.container)}>
+        <h1>Heimdallr</h1>
+      </div>
+    </Layout>
   );
 }
--- apps/thor/src/pages/_app.tsx
+++ apps/thor/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/global.css";
+import "../scripts/wdyr";
 
 import type { AppProps } from "next/app";
 
