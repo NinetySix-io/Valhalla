461d5149616d18a85c5a3d478b7b56b648428476
An Duong <an.duong@budee.org>
2022-04-14 23:28:16 -0400

    Add tailwind

---
+++ apps/heimdallr/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import cx from "clsx";
+import styles from "../styles/home.module.css";
+
+export default function Home() {
+  return (
+    <div className={cx(styles.container)}>
+      <h1>Heimdallr</h1>
+    </div>
+  );
+}
