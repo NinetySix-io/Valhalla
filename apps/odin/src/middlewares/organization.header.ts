8f3f7ba0af996c1d4b0f1ae04097de9fb0132e96
An Duong <an.duong@budee.org>
2022-04-14 20:15:31 -0400

    wip

--- apps/odin/src/middlewares/organization.header.ts
+++ apps/odin/src/middlewares/organization.header.ts
@@ -1,6 +1,5 @@
 import type { NextFunction, Request, Response } from 'express';
 
-import { BadRequestException } from '@nestjs/common';
 import { Environment } from '@odin/config/environment';
 import { isObjectId } from '@odin/lib/is.objectid';
 import mongoose from 'mongoose';
@@ -9,11 +8,10 @@ export function organizationHeader() {
   return (request: Request, _response: Response, next: NextFunction) => {
     const identifier = Environment.variables.ORGANIZATION_HEADER_IDENTIFIER;
     const requestOrg = request.headers[identifier];
-    if (!isObjectId(requestOrg)) {
-      throw new BadRequestException('Invalid organization data type');
-    }
+    request.organization = isObjectId(requestOrg)
+      ? new mongoose.Types.ObjectId(requestOrg)
+      : undefined;
 
-    request.organization = new mongoose.Types.ObjectId(requestOrg);
     return next();
   };
 }
