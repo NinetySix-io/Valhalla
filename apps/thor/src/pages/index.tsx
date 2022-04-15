7047b17457e1377e18c2d560fa7b37088e8e6ed5
An Duong <an@ninetysix.io>
2022-04-14 23:28:16 -0400

    Add tailwind

--- apps/thor/src/pages/index.tsx
+++ apps/thor/src/pages/index.tsx
@@ -1,4 +1,4 @@
-export default function Web() {
+export default function Home() {
   return (
     <div>
       <h1>Thor</h1>
---
+++ packages/config/tailwind.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+module.exports = {
+  content: ["../../packages/ui/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
