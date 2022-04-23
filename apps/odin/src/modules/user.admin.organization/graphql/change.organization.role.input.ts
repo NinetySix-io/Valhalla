88b776df1450d8fd4212831c96049007148f88ae
An Duong <an.duong@budee.org>
2022-04-23 16:41:57 -0400

    w

--- apps/odin/src/modules/user.admin.organization/graphql/change.organization.role.input.ts
+++ apps/odin/src/modules/user.admin.organization/graphql/change.organization.role.input.ts
@@ -1,15 +1,16 @@
-import { Field, InputType, ObjectType, PickType } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
 
+import { IsEnum } from 'class-validator';
 import { IsObjectId } from '@odin/lib/class.validators/is.object.id';
-import { UserMembershipSchema } from '@odin/data.models/user.memberships/schema';
+import { UserMembershipRole } from '@odin/data.models/user.memberships/schema';
 
 @InputType()
-export class ChangeOrganizationRoleInput extends PickType(
-  UserMembershipSchema,
-  ['role'],
-  ObjectType,
-) {
+export class ChangeOrganizationRoleInput {
   @Field({ description: 'User ID' })
   @IsObjectId()
   user: string;
+
+  @Field(() => UserMembershipRole, { description: 'User role within group' })
+  @IsEnum(UserMembershipRole)
+  role: UserMembershipRole;
 }
