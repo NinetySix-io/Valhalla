5e013a38ebe7aebfd549f2513b710e71116bf557
An Duong <an.duong@budee.org>
2022-04-24 11:24:56 -0400

    Valhalla: Update deployment script

--- apps/loki/.eslintrc.js
+++ apps/loki/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  extends: ["../../packages/config/eslint-react"],
+  extends: ['@valhalla/config/eslint-react'],
   rules: {
-    "react/react-in-jsx-scope": "off",
+    'react/react-in-jsx-scope': 'off',
   },
 };
