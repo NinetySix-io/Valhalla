ed53857951232e2a2d9e216e7f0312f690e7ee70
An Duong <an.duong@budee.org>
2022-04-24 11:40:43 -0400

    Valhalla: Fix lint

--- apps/thor/.eslintrc.js
+++ apps/thor/.eslintrc.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 module.exports = {
-  extends: ['@valhalla/config/eslint-react'],
+  extends: ['../../packages/config/eslint-react'],
   rules: {
     'react/react-in-jsx-scope': 'off',
   },
