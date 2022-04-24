16d09da2a68dc2951804ffc71c7012a1a911df2c
An Duong <an.duong@budee.org>
2022-04-24 00:09:48 -0400

    w

--- apps/loki/src/pages/_document.tsx
+++ apps/loki/src/pages/_document.tsx
@@ -18,7 +18,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
           {/* Inject MUI styles first to match with the prepend: true configuration. */}
-          {(this.props as any).emotionStyleTags}
+          {this.props["emotionStyleTags"]}
         </Head>
         <body>
           <Main />
