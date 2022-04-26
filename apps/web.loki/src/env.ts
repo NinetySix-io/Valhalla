3bad6123de02ed2bfe741fdef8d46d5c7737a07f
An Duong <an@ninetysix.io>
2022-04-25 21:15:35 -0400

    Web: Add next runtime vars

--- apps/web.loki/src/env.ts
+++ apps/web.loki/src/env.ts
@@ -1,9 +1,14 @@
 import * as Yup from 'yup';
 
 import { buildEnvironment } from '@valhalla/utilities/dist/env';
+import getConfig from 'next/config';
 
+const { publicRuntimeConfig } = getConfig();
 const schema = Yup.object({
   SERVER: Yup.string().default('http://localhost:3002'),
 });
 
-export class Environment extends buildEnvironment({ schema }) {}
+export class Environment extends buildEnvironment({
+  schema,
+  vars: publicRuntimeConfig(),
+}) {}
