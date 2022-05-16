cee6869eb5f34a29a05667411a42367b34261dc9
An Duong <an.duongx@gmail.com>
2022-05-16 02:32:07 -0400

    Fix web build

--- apps/web.loki/src/env.ts
+++ apps/web.loki/src/env.ts
@@ -1,16 +1,17 @@
 import * as Yup from 'yup';
 
 import { buildEnvironment } from '@valhalla/utilities/dist/env';
-import getConfig from 'next/config';
 
-const { publicRuntimeConfig } = getConfig();
+// import getConfig from 'next/config';
+
+// const { publicRuntimeConfig } = getConfig();
 const schema = Yup.object({
   SERVER: Yup.string().default('http://localhost:3002'),
 });
 
 export class Environment extends buildEnvironment({
   schema,
-  vars: publicRuntimeConfig(),
+  // vars: publicRuntimeConfig(),
 }) {
   get isServer() {
     return typeof window === 'undefined';
