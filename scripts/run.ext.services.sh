f955b71c9bf20d59c7ec766edb8305b2aeefe111
An Duong <an.duongx@gmail.com>
2022-05-24 01:19:43 -0400

    Valhalla: Move docker files

--- scripts/run.ext.services.sh
+++ scripts/run.ext.services.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker-compose --project-name=valhalla-ext --project-directory=../ -f docker-compose.ext.yml up -d
+docker-compose --project-name=valhalla-ext --project-directory=../ -f ./docker/docker-compose.ext.yml up -d
