780000722f02512db1d589a5f87cb59c6bc343a8
An Duong <an.duongx@gmail.com>
2022-06-25 15:35:15 -0400

    packages.utilities: Add omit, omitBy, pick, pickBy

--- packages/utilities/src/index.ts
+++ packages/utilities/src/index.ts
@@ -9,6 +9,8 @@ export { default as throttle } from 'lodash.throttle';
 export { default as debounce } from 'lodash.debounce';
 export { default as pick } from 'lodash.pick';
 export { default as omit } from 'lodash.omit';
+export { default as pickBy } from 'lodash.pickby'
+export { default as omitBy } from 'lodash.omitby'
 export * as SStruct from 'superstruct';
 export * from './validators';
 export * from './make.tag';
