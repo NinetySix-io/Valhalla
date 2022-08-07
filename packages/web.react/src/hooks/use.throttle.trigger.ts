ba00f760e946db8509e1b90e042404e9b3489d60
An Duong <an.duongx@gmail.com>
2022-08-07 04:20:50 -0400

    [web.react] refactor use.throttle.trigger

--- packages/web.react/src/hooks/use.throttle.trigger.ts
+++ packages/web.react/src/hooks/use.throttle.trigger.ts
@@ -1,41 +1,14 @@
-import React, { DependencyList, useEffect, useRef, useState } from 'react';
+import React, { DependencyList } from 'react';
 
-export const useThrottleTrigger = <T, U extends DependencyList>(
-  fn: (...args: U) => T,
+import { throttle } from '@valhalla/utilities';
+
+export const useThrottleTrigger = (
+  fn: () => void,
   ms = 200,
-  deps: U,
+  deps: DependencyList,
 ) => {
-  const [state, setState] = useState<T | null>(null);
-  const timeout = useRef<ReturnType<typeof setTimeout>>();
-  const nextArgs = useRef<U>();
+  const trigger = React.useCallback(() => throttle(() => fn(), ms)(), [ms, fn]);
 
-  useEffect(() => {
-    if (!timeout.current) {
-      setState(fn(...deps));
-      const timeoutCallback = () => {
-        if (nextArgs.current) {
-          setState(fn(...nextArgs.current));
-          nextArgs.current = undefined;
-          timeout.current = setTimeout(timeoutCallback, ms);
-        } else {
-          timeout.current = undefined;
-        }
-      };
-      timeout.current = setTimeout(timeoutCallback, ms);
-    } else {
-      nextArgs.current = deps;
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, deps);
-
-  React.useEffect(() => {
-    return () => {
-      if (timeout.current) {
-        clearTimeout(timeout.current);
-      }
-    };
-  });
-
-  return state;
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  React.useEffect(trigger, deps);
 };
