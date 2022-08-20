91910dae5fc02f68f88d32731ca03db4c7eaa41b
An Duong <an.duongx@gmail.com>
2022-08-20 19:04:26 -0400

    [web.builder] Update drop grid background

--- packages/web.builder/src/drop.grid/background.tsx
+++ packages/web.builder/src/drop.grid/background.tsx
@@ -10,13 +10,11 @@ const Container = styled(
   makeFilterProps(['isVisible']),
 )<{ isVisible: boolean }>(
   ({ theme, isVisible }) => css`
-    --bg-pos: calc((var(--cs) / 2) + var(--pt-w));
+    --bg-pos: (var(--cs) + var(--pt-w));
     --bg-color: ${theme.palette.grey[600]};
     opacity: ${isVisible ? 0.5 : 0};
     width: 100%;
     height: 100%;
-    transition: all 0.2s;
-    transition-timing-function: ease;
     position: absolute;
     top: 0;
     left: 0;
@@ -25,6 +23,9 @@ const Container = styled(
       radial-gradient(var(--bg-color) var(--pt-w), transparent 3px), #fff;
     background-position: calc(var(--bg-pos)) calc(var(--bg-pos));
     background-size: var(--cs) var(--cs);
+    transition: ${theme.transitions.create(['all'], {
+      duration: theme.transitions.duration.shortest,
+    })};
   `,
 );
 
