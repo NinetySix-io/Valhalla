423df14c704859941ce14778ca2b12e120e9a241
An Duong <an.duongx@gmail.com>
2022-09-13 21:59:51 -0400

    [*] Remove template
    no time to maintain it

--- templates/serv.app/src/grpc/grpc.controller.ts
+++
@@ -1,8 +0,0 @@
-import { SITES_SERVICE_NAME, SitesServiceController } from '@app/protobuf';
-
-import { Controller } from '@nestjs/common';
-import { GrpcClass } from '@valhalla/serv.core';
-
-@Controller()
-@GrpcClass(SITES_SERVICE_NAME)
-export class gRpcController implements SitesServiceController {}
--- templates/serv.app/src/services/boot.config.service.ts
+++
@@ -1,5 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { ServAppConfigService } from '@valhalla/serv.core';
-
-@Injectable()
-export class BootConfigService extends ServAppConfigService {}
